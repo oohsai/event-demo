361a95a609126114bf13d666079d5c55359955eb
oohsai <s72454726@gmail.com>
2024-02-14 05:40:36 +0530

    Inital clerk done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "../globals.css";
-import Header from "@/components/ui/shared/Header";
-import Footer from "@/components/ui/shared/Footer";
+import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
 const poppins = Poppins({
   subsets: ["latin"],
